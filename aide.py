f25622d3c9aa9f853f11193fe768663b7dd7b730
furusunmk2 <230084cq@yse-c.net>
2024-11-27 10:20:30 +0900

    first commit

--- aide.py
+++ aide.py
@@ -7,8 +7,5 @@ patient_num = input()
 for i in range(8):
     if patient.patient[i].startswith(patient_num):
         print(patient.patient[i])
-
-        print(patient.patient_data[i][0][int(random.randint(0, len(patient.patient_data[i])))])
-        print(patient.patient_data[i][1][int(random.randint(0, len(patient.patient_data[i])))])
-        print(patient.patient_data[i][2][int(random.randint(0, len(patient.patient_data[i])))])
-        print(patient.patient_data[i][3][int(random.randint(0, len(patient.patient_data[i])))])
+        for j in range(len(patient.patient_data[i])):
+            print(patient.patient_data[i][j][int(random.randint(0, len(patient.patient_data[i][j])-1))])
