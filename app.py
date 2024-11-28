47a0b0e3d5781ac88a8f52b4261726c77e2828cf
furusunmk2 <230084cq@yse-c.net>
2024-11-28 10:18:32 +0900

    first commit

--- app.py
+++ app.py
@@ -75,4 +75,7 @@ def generate_patient_response(indices):
     return response_text
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
