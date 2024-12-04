e9929964cce6a36c17b0ff21f46896f51156b5eb
furusunmk2 <230084cq@yse-c.net>
2024-12-04 09:46:03 +0900

    first commit

--- app.py
+++ app.py
@@ -51,45 +51,16 @@ def handle_message(event):
     patient_num = event.message.text.strip()  # ユーザーの入力を取得
     response_text = ""
 
-    if patient_num == "きずな":
-        response_text = generate_patient_response(range(4))
-    elif patient_num == "つなぐ":
-        response_text = generate_patient_response(range(4, 8))
-    else:
-        matched = False
-        for i in range(8):
-            if patient.patient[i].startswith(patient_num):
-                response_text = generate_patient_response([i])
-                matched = True
-                break
-        if not matched:
-            response_text = "該当するデータが見つかりません。"
-
+    if len(patient_num) % 2 == 0:
+        response_text = "渡辺です"
+    else:    
+        response_text = "富永です"
+ 
     # ユーザーに返信
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=response_text)
     )
-
-def generate_patient_response(indices):
-    """
-    患者情報を生成する関数
-    """
-    response_data = []
-    for k in indices:
-        patient_info = {"name": patient.patient[k], "data": []}
-        for j in range(len(patient.patient_data[k])):
-            random_data = random.choice(patient.patient_data[k][j])
-            patient_info["data"].append(random_data)
-        response_data.append(patient_info)
-
-    # テキスト形式に変換
-    response_text = "\n".join([
-        f"{info['name']}:\n " + "\n ".join(info['data']) for info in response_data
-    ])
-
-
-    return response_text
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
